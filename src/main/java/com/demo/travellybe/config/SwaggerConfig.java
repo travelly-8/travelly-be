0a3950a6ad44073b9ec879d275768c4d503f5e4b
JeongGiSeong <rltjd8714@gmail.com>
2024-05-22 11:39:20 +0900

    feat: config 폴더 위치 변경 및 QueryDslConfig, SwaggerConfig 추가

---
+++ src/main/java/com/demo/travellybe/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.demo.travellybe.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Travelly API")
+                        .description("Swagger API 문서입니다.")
+                        .version("1.0.0"));
+//                .components(new Components()
+//                        .addSecuritySchemes("basicAuth",
+//                                new io.swagger.v3.oas.models.security.SecurityScheme()
+//                                        .type(SecurityScheme.Type.HTTP)
+//                                        .scheme("basic")));
+    }
+}
