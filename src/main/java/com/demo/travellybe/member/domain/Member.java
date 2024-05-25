814813562facd384410fcd76c56ac5903225a2ab
WonJoon <seowj@gmail.com>
2024-05-25 20:10:30 +0900

    feat: 후기 엔티티 생성 및 연관관계 설정

--- src/main/java/com/demo/travellybe/member/domain/Member.java
+++ src/main/java/com/demo/travellybe/member/domain/Member.java
@@ -1,12 +1,16 @@
 package com.demo.travellybe.member.domain;
 
 import com.demo.travellybe.member.domain.Role;
+import com.demo.travellybe.review.domain.Review;
 import com.demo.travellybe.util.BaseTimeEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor
@@ -26,7 +30,14 @@ public class Member extends BaseTimeEntity {
     @Column(nullable = false)
     private String nickname;
 
+    private int coin = 0;
+
+    private String imageUrl;
+
     private Role role;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
 
     @Builder
     public Member(String email, String password, String nickname) {
