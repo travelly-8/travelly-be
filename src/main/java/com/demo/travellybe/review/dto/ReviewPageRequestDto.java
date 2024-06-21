176e09922f18218b7af0dfad6dae15762baa5e3f
JeongGiSeong <rltjd8714@gmail.com>
2024-06-21 14:02:34 +0900

    fix: 오류 수정

--- src/main/java/com/demo/travellybe/review/dto/ReviewPageRequestDto.java
+++ src/main/java/com/demo/travellybe/review/dto/ReviewPageRequestDto.java
@@ -10,21 +10,23 @@ import org.springframework.data.domain.Sort;
 @Data
 public class ReviewPageRequestDto {
     @NotNull
+    @Schema(example = "0")
     private int page;
     @NotNull
+    @Schema(example = "10")
     private int size;
-    @Schema(description = "정렬 방식", example = "new/old/low-rating/high-rating")
+    @Schema(description = "정렬 방식", example = "new/old/lowRating/highRating")
     private String sort = "new";
 
     public Pageable toPageable() {
         // new는 최신순, old는 오래된순, low-rating은 낮은 평점순, high-rating은 높은 평점순
         String sortField = switch (sort) {
-            case "low-rating", "high-rating" -> "rating";
+            case "lowRating", "highRating" -> "rating";
             default -> "createdDate";
         };
 
         Sort sort;
-        if (this.sort.equals("low-rating") || this.sort.equals("old")) {
+        if (this.sort.equals("lowRating") || this.sort.equals("old")) {
             sort = Sort.by(sortField).ascending();
         } else {
             sort = Sort.by(sortField).descending();
